14f4c5f649b4e0814d57fe8460bbe4a3923adac6
Evan Porter <evan.porter@dotman.ca>
2021-01-04 21:53:28 -0400

    Sort of working implementation of spring security resource server

--- src/main/groovy/ca/evanepio/autheaxmple/dualauth/controllers/PieController.groovy
+++ src/main/groovy/ca/evanepio/autheaxmple/dualauth/controllers/PieController.groovy
@@ -1,8 +1,5 @@
 package ca.evanepio.autheaxmple.dualauth.controllers
 
-import org.keycloak.KeycloakPrincipal
-import org.keycloak.KeycloakSecurityContext
-import org.keycloak.representations.AccessToken
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,11 +12,6 @@ class PieController {
 
     @GetMapping("greeting")
     String greeting(Principal principal) {
-        KeycloakPrincipal<KeycloakSecurityContext> kp = (KeycloakPrincipal<KeycloakSecurityContext>) principal
-        AccessToken token = kp.getKeycloakSecurityContext().getToken()
-
-        String email = token.getEmail()
-
-        return "Hello and have some pie, ${token.getName()}! $email"
+        return "Hello and have some pie, Rando Calrissian!"
     }
 }
