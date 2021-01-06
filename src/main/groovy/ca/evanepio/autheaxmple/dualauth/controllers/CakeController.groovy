fdc6c53ea13073e22c5b3ce65fac7d20613e8713
Evan Porter <evan.porter@dotman.ca>
2021-01-05 20:42:40 -0400

    Use parts of the token to generate response

--- src/main/groovy/ca/evanepio/autheaxmple/dualauth/controllers/CakeController.groovy
+++ src/main/groovy/ca/evanepio/autheaxmple/dualauth/controllers/CakeController.groovy
@@ -1,5 +1,6 @@
 package ca.evanepio.autheaxmple.dualauth.controllers
 
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,6 +13,9 @@ class CakeController {
 
     @GetMapping("greeting")
     String greeting(Principal principal) {
-        return "Have some cake, Rando!"
+        JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) principal
+        def email = jwtToken.getTokenAttributes()['preferred_username']
+        def name = jwtToken.getTokenAttributes()['name']
+        return "Hello and have some cake, $name! $email"
     }
 }
