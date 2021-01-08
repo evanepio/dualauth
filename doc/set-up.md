8198c6b2338d9f7ecac463468d5adb964af00cd7
Evan Porter <evan.porter@dotman.ca>
2021-01-07 21:42:55 -0400

    Ad two realms to log in with, but lose role based access

--- doc/set-up.md
+++ doc/set-up.md
@@ -3,7 +3,8 @@
 1. Run `docker-compose up -d`
 2. Go to http://localhost:8080 and log in as `admin` / `admin`
 3. Import `realm-one-export.json`
+4. Import `realm-two-export.json`
 
 # Use Insomnia
 
-Open Insomnia and import the workspace `Insomnia_2021-01-05.json`
+Open Insomnia and import the workspace `Insomnia_workspace.json`
--- src/main/groovy/ca/evanepio/autheaxmple/dualauth/configs/SecurityConfig.groovy
+++ src/main/groovy/ca/evanepio/autheaxmple/dualauth/configs/SecurityConfig.groovy
@@ -1,49 +1,38 @@
 package ca.evanepio.autheaxmple.dualauth.configs
 
-import ca.evanepio.autheaxmple.dualauth.KeycloakRealmRoleConverter
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.core.convert.converter.Converter
 import org.springframework.http.HttpMethod
-import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver
 
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        def authenticationManagerResolver = new JwtIssuerAuthenticationManagerResolver(
+                'http://localhost:8080/auth/realms/one',
+                'http://localhost:8080/auth/realms/two'
+        )
+
         // @formatter:off
         http
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and()
+                .cors( cors ->
+                    cors.disable()
+                )
+                .sessionManagement( sessionManagement ->
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeRequests((authorizeRequests) ->
                         authorizeRequests
-                                .antMatchers(HttpMethod.GET, "/pie/eat/**", "/cake/eat/**").hasRole('user')
+                                .antMatchers(HttpMethod.GET, "/pie/eat/**", "/cake/eat/**").authenticated()
                                 .antMatchers(HttpMethod.GET, "/pie/bake/**", "/cake/bake/**").hasRole('admin')
                                 .anyRequest().permitAll()
                 )
-                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter())
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                    oauth2ResourceServer.authenticationManagerResolver(authenticationManagerResolver)
+                )
         // @formatter:on
-    }
-
-    @Bean
-    JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build()
-    }
-
-    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter()
-        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter())
-        return jwtConverter
     }
 }
